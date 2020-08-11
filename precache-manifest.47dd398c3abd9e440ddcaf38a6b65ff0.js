cbfdd752e0f51183b90449faf943efea77cf51da
Daniel Nwokocha <gurudanito001@gmail.com>
2020-08-11 09:50:57 +0100

    Updates

---
+++ precache-manifest.47dd398c3abd9e440ddcaf38a6b65ff0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae357038d6476073ee4f303e7dc0a36b",
+    "url": "/stocktaker/index.html"
+  },
+  {
+    "revision": "0ba901ab7e82fc29d4a7",
+    "url": "/stocktaker/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "08e8bea566f91ff4abdf",
+    "url": "/stocktaker/static/css/main.d367501d.chunk.css"
+  },
+  {
+    "revision": "0ba901ab7e82fc29d4a7",
+    "url": "/stocktaker/static/js/2.774f02ec.chunk.js"
+  },
+  {
+    "revision": "139a99c3bcb770db506be6733f348e74",
+    "url": "/stocktaker/static/js/2.774f02ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e8bea566f91ff4abdf",
+    "url": "/stocktaker/static/js/main.4fd7fc9e.chunk.js"
+  },
+  {
+    "revision": "f4fc089327d639424469",
+    "url": "/stocktaker/static/js/runtime-main.d2dbe71b.js"
+  },
+  {
+    "revision": "0d4ae5c6769abfc356964248fe59974c",
+    "url": "/stocktaker/static/media/del.0d4ae5c6.svg"
+  },
+  {
+    "revision": "387b1df0ad5882a82807bd7eabcea810",
+    "url": "/stocktaker/static/media/edit.387b1df0.svg"
+  },
+  {
+    "revision": "19c24aff0329371dd23c24d6385fd3d5",
+    "url": "/stocktaker/static/media/history.19c24aff.svg"
+  },
+  {
+    "revision": "6651dd8a42528b2fa2edbfc781d3ac85",
+    "url": "/stocktaker/static/media/menu-white.6651dd8a.svg"
+  },
+  {
+    "revision": "8e73484fae77bcc1b78380906a28abfe",
+    "url": "/stocktaker/static/media/menu.8e73484f.svg"
+  },
+  {
+    "revision": "5d14e7e5475ebf20459756d6f9b93912",
+    "url": "/stocktaker/static/media/more-options.5d14e7e5.svg"
+  },
+  {
+    "revision": "8ad4201097b21754a7e7222ca6229e42",
+    "url": "/stocktaker/static/media/spareparts.8ad42010.svg"
+  },
+  {
+    "revision": "2d9fe0cf5b8774b4fe53b25dd5745d67",
+    "url": "/stocktaker/static/media/truck.2d9fe0cf.svg"
+  },
+  {
+    "revision": "15eeae14a20ce229ca17f47f16386077",
+    "url": "/stocktaker/static/media/user.15eeae14.svg"
+  }
+]);
